f350a6edcad667053da9f1802798afb3a59bd422
Orlando312 <dso2025955@est.univalle.edu>
2025-06-11 06:31:31 -0400

    Subiendo pagina web

---
+++ js/javaScript.js
@@ -0,0 +1,4 @@
+document.getElementById("btnSaludo").addEventListener("click", function () {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.textContent = "¡Hola! Gracias por visitar mi página 😄";
+  });  
